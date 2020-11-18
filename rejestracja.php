f621663dc4fdc8a326d67a2fe81e5dfdbbf00d2e
Kuba Przybysz <kuba.kubis@interia.pl>
2020-11-18 11:18:48 +0100

    up

--- rejestracja.php
+++ rejestracja.php
@@ -152,69 +152,6 @@
 	<script src='https://www.google.com/recaptcha/api.js'></script>
 	<link rel="icon" type="image/png" href="photo/logo.png">
 	<style>
-	body{
-      margin: 0;
-      padding: 0;
-      font-family: sans-serif;
-      background: #34495e;
-      color: white;
-    }
-    .box{
-      width: 300px;
-      padding: 40px;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%,-50%);
-      background: #191919;
-      text-align: center;
-    }
-    .box h1{
-      color: white;
-      text-transform: uppercase;
-      font-weight: 500;
-    }
-    .box input[type = "text"],.box input[type = "password"]{
-      border:0;
-      background: none;
-      display: block;
-      margin: 20px auto;
-      text-align: center;
-      border: 2px solid #3498db;
-      padding: 14px 10px;
-      width: 200px;
-      outline: none;
-      color: white;
-      border-radius: 24px;
-      transition: 0.25s;
-    }
-    .box input[type = "text"]:focus,.box input[type = "password"]:focus{
-      width: 280px;
-      border-color: #2ecc71;
-    }
-    .box input[type = "submit"]{
-      border:0;
-      background: none;
-      display: block;
-      margin: 20px auto;
-      text-align: center;
-      border: 2px solid #2ecc71;
-      padding: 14px 40px;
-      outline: none;
-      color: white;
-      border-radius: 24px;
-      transition: 0.25s;
-      cursor: pointer;
-    }
-    .box input[type = "submit"]:hover{
-      background: #2ecc71;
-    }
-		.error
-		{
-			color:red;
-			margin-top: 10px;
-			margin-bottom: 10px;
-		}
 		.g-recaptcha{
 		margin-top:20px;
 		}
@@ -314,7 +251,7 @@
 		<br />
 		
 		<input type="submit" value="Zarejestruj się" />
-		<a href="index.php">Masz już konto?</a>
+		<a href="zalform.php">Masz już konto?</a>
 		
 	</form>
 
