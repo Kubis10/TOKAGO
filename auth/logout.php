cc71527ca16cb3f2bb6379312f3f52b9c5b3dfa2
Adrian Goral <adrian.goral@gmail.com>
2020-11-23 18:01:20 +0100

    Changed logout

--- auth/logout.php
+++ auth/logout.php
@@ -4,14 +4,16 @@
 
 	require_once "../res/connect.php";
 
-	if ($polaczenie->connect_errno!=0){}
-    	echo "Error: ".$polaczenie->connect_errno;
+	if ($polaczenie->connect_errno!=0){
+		echo "Error: ".$polaczenie->connect_errno;
+	}
+    	
 
 	$idGracza = $_SESSION['id'];
 
     if($polaczenie->query("INSERT INTO logs VALUES (NULL, 'Gracz o id = ".$idGracza." wylogowal sie',now())")){
-    session_unset();
-	header('Location: ../index.html');
+    	session_unset();
+		header('Location: ../index.html');
     }
 
 ?>
