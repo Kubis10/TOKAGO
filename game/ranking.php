f5582faa93f92d60c19caa1414bd27ebd1ee5a20
Kuba Przybysz <kuba.kubis@interia.pl>
2020-11-23 20:05:45 +0100

    update

--- game/ranking.php
+++ game/ranking.php
@@ -20,26 +20,17 @@ try
                        if($rezultat->num_rows>0){
                             $aktualny_rekord = $rezultat->fetch_assoc();
 
-                            if ((float)$aktualny_rekord["l".$level]>$newTime && (float)$aktualny_rekord["l".$level] != 0){
-
-	                                
-	                            if($polaczenie->query("INSERT INTO logs VALUES (NULL, 'Gracz o id = ".$row['id']." ustanowil rekord poziomu
-	                            	'".$aktualny_rekord["l".$level]."', now())")){
-
-	                            	}
-
-                            }
-
-
                             if((float)$aktualny_rekord["l".$level]>$newTime || (float)$aktualny_rekord["l".$level] == 0){
+                            if($polaczenie->query("INSERT INTO logs VALUES (NULL, 'Gracz o id = ".$row['id']." ustanowil rekord poziomu".$aktualny_rekord["l".$level]."', now())")){
                                 if ($polaczenie->query("UPDATE rank SET l".$level."='$newTime' WHERE id_gracza='$id'"))
-                                {s
+                                {
                                     $_SESSION['udanyzapisrank']=true;
                                 }
                                 else
                                 {
                                     throw new Exception($polaczenie->error);
                                 }
+                            }
                             }
 				       }
 				       else
