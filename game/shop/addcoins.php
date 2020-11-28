db4177761ad6e86ae06520b2bc7f9eb7d6b9788c
Kuba Przybysz <kuba.kubis@interia.pl>
2020-11-28 19:42:19 +0100

    shop update

--- game/shop/addcoins.php
+++ game/shop/addcoins.php
@@ -5,33 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>TOKAGO | Dodaj Tokelony</title>
     <link rel="icon" type="image/png" href="../../res/photo/logo.png">
-    <link rel="stylesheet" href="css/shop.css"/>
+    <link rel="stylesheet" href="../../res/css/shop.css"/>
 </head>
-<body>
-
+<body class="bodycoin">
+<h1 class="header">Dodaj Tokelony</h1>
 <form action="checkout.php" method="post">
     <input type="hidden" name="price" value="5.99"></input>10
-    <input type="submit"></input>
+    <button type="submit"></button>
 </form>
 <form action="checkout.php" method="post">
     <input type="hidden" name="price" value="10.99"></input>20
-    <input type="submit"></input>
+    <button type="submit"></button>
 </form>
 <form action="checkout.php" method="post">
     <input type="hidden" name="price" value="25.99"></input>50
-    <input type="submit"></input>
+    <button type="submit"></button>
 </form>
 <form action="checkout.php" method="post">
     <input type="hidden" name="price" value="44.99"></input>100
-    <input type="submit"></input>
+    <button type="submit"></button>
 </form>
 <form action="checkout.php" method="post">
     <input type="hidden" name="price" value="139.99"></input>300
-    <input type="submit"></input>
+    <button type="submit"></button>
 </form>
 <form action="checkout.php" method="post">
     <input type="hidden" name="price" value="249.99"></input>500
-    <input type="submit"></input>
+    <button type="submit"></button>
 </form>
 </body>
 </html>
