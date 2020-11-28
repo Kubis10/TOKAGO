a04ba3a0ad3a120539dad7cff674620c2a422ba2
Kuba Przybysz <kuba.kubis@interia.pl>
2020-11-28 16:39:27 +0100

    shop update

---
+++ game/shop/addcoins.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TOKAGO | Dodaj tokelony</title>
+    <link rel="icon" type="image/png" href="../res/photo/logo.png">
+    <link rel="stylesheet" href="css/shop.css"/>
+</head>
+<body>
+<a>10, 5.99</a>
+<a>20, 10.99</a>
+<a>50, 25.99</a>
+<a>100, 44.99</a>
+<a>300, 139.99</a>
+<a>500, 249.99</a>
+</body>
+</html>
