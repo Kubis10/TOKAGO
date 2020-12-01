66210f1861db4786be5765c0accb8dc21ab31cb0
Kuba Przybysz <kuba.kubis@interia.pl>
2020-12-01 17:43:13 +0100

    add eq

--- game/eq.php
+++ game/eq.php
@@ -1,2 +1,14 @@
-<?
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+    <link rel="icon" type="image/png" href="../res/photo/logo.png">
+    <title>TOKAGO | Ekwipunek</title>
+    <link rel="stylesheet" href="../res/css/eq.css"/>
+    <link rel="stylesheet" href="../res/css/main.css"/>
+</head>
+<body>
 
+</body>
+</html>
