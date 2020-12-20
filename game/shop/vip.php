594eeb02d0446473d2317bc692255a94a7a3c261
Kuba Przybysz <kuba.kubis@interia.pl>
2020-12-20 21:27:02 +0100

    update

--- game/shop/vip.php
+++ game/shop/vip.php
@@ -86,7 +86,7 @@ if ($checkBalance->num_rows > 0) {
                     <div class="card__details">
                         <ul>
                             <li>1 Rok premium</li>
-                            <li>5 tokelonów co tydzień</li>
+                            <li>4 tokelonów co tydzień</li>
                             <li>20 tokelonów na start</li>
                             <li>Specjalny skin</li>
                             <li>Specjalne wyróżnienie w rankingu</li>
